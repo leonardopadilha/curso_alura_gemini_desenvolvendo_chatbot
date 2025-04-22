4d290c9e4d1ab5a906c9762bbd48a934c8c00277
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-04-22 12:38:48 -0300

    Leitura de arquivos com embeddings

--- chat.js
+++ chat.js
@@ -1,12 +1,16 @@
 
-import { incorporarDocumentos, incorporarPergunta } from './embedding.js';
+import { incorporarDocumentos, incorporarPergunta, leArquivos } from './embedding.js';
 import { chat, funcoes } from './inicializaChat.js'
 
-const documentos = await incorporarDocumentos(
+const arquivos = await leArquivos(["Pacotes_Argentina.txt", "Pacotes_EUA.txt", "Politicas.txt"])
+
+const documentos = await incorporarDocumentos(arquivos)
+
+/* const documentos = await incorporarDocumentos(
   ["A política de cancelamento é de 30 dias antes da viagem, caso contrário, não faremos o reembolso",
    "Viagem para a Disney 6 dias, R$ 20.000,00 - Viagem para a Disney 10 dias, R$ 25.000,00"
   ]
-)
+) */
 
 //console.log(documentos)
 
